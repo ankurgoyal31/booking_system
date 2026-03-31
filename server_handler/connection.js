db4f7dda5eb0d4a858d4b67fb9d161a73af84d44
ankurgoyal31 <ankurgoyal1227@gmail.com>
2026-04-01 01:24:17 +0530

    Update connection.js

--- server_handler/connection.js
+++ server_handler/connection.js
@@ -1,13 +1,27 @@
-import mysql from "mysql2/promise"
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
+let db;
+
 async function connect() {
-  const db = await mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  multipleStatements: true 
-});
-  return db;
-} 
-  
+  try {
+    db = await mysql.createConnection({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT, 
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: {
+        rejectUnauthorized: false
+      },
+       multipleStatements: true
+    });
+    return db;
+
+  } catch (err) {
+    console.log("DB Error:", err);
+  }
+}
+
 export default connect;
