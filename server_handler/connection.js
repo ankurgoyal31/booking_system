3cb4df566b43cdc9b314ce73d49ca800d389d93b
ankurgoyal31 <ankurgoyal1227@gmail.com>
2026-03-31 20:48:36 +0530

    Update connection.js

--- server_handler/connection.js
+++ server_handler/connection.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2/promise"
 async function connect() {
   const db = await mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "ankur@631",
-  database: "events",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   multipleStatements: true 
 });
   return db;
