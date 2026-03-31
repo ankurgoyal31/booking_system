e496959fab269162f43cc225ce1aba2cc23aad57
ankurgoyal31 <ankurgoyal1227@gmail.com>
2026-04-01 01:22:07 +0530

    Update users_data.js

--- server_handler/users_data.js
+++ server_handler/users_data.js
@@ -1,31 +1,27 @@
 import express from "express";
 import connect from "./connection.js";
-import { check_event,check_user,check_user_ticket,events_status,check_user_booking,check_email_exists} from "./varification.js";
-import fs from "fs"
-import { run_sql } from "./run_sql.js";
-const  sql = fs
-const app = express();
-await run_sql();
-app.use(express.json())
 const db = await connect(); 
- 
-app.post("/user",check_email_exists,async(req,res)=>{
+import { check_event,check_user,check_user_ticket,events_status,check_user_booking,check_email_exists} from "./varification.js";
+const app = express(); 
+ app.use(express.json()) 
+ app.post("/user",check_email_exists,async(req,res)=>{
     try{
 const [user] = await db.query("insert into users (name,email) values (?, ?)",[req.body.name, req.body.email]);
 console.log(user.id)
-res.send({message:"user sucessfully created, please use this user_Id to booking or create the events",user_id:user.insertId})
+res.status(200).json({message:"user sucessfully created, please use this user_Id to booking or create the events",user_id:user.insertId})
     }catch(err){
-        res.send({message:'something went wrong',err:err});
+        res.status(400).json({message:"something went wrong.."}) 
     }    
-}) 
+})  
 
 app.post("/events",check_user, async(req,res)=>{
     try{
-let [events] = await db.query(`insert into events (status,title, description,date, total_capacity, remaining_tickets, user_id)  values (?,?, ?, ?, ?, ?, ?)`
-    ,[true,req.body.title,req.body.description,req.body.date,req.body.total_capacity,req.body.remaining_tickets, req.body.user_id]);
-res.send({message:'your event created sucessfully,please use this event_id for booking',event_id:events.insertId})
+let [events] = await db.query(`insert into events (status,title, description,date, total_capacity,remaining_tickets, user_id)  values (?,?, ?, ?, ?, ?, ?)`
+    ,[true,req.body.title,req.body.description,req.body.date,req.body.total_capacity,req.body.total_capacity, req.body.user_id]);
+res.status(200).json({message:'your event created sucessfully,please use this event_id for booking',event_id:events.insertId})
     }catch(err){
-        res.send({message:'something went wrong....',create:false,err:err})
+        console.log(err)
+        res.status(400).json({message:"something went wrong..",err:err}) 
     }
 })
 
@@ -34,31 +30,30 @@ app.post("/booking_ticket",check_user,check_event,events_status,check_user_ticke
 let [update] = await db.query("UPDATE events SET remaining_tickets = remaining_tickets - 1 WHERE id = ? AND remaining_tickets > 0",[req.body.event_id]);
 if(update.affectedRows==0){
 await db.rollback(); 
-return res.send({ message: "Tickets sold out" });
+return res.status(400).json({ message: "Tickets sold out" });
 }
-
 let [booking] = await db.query("insert into bookings (user_id,event_id) values (?,?)",[req.body.user_id,req.body.event_id])
-res.send({message:"your booking confirmed....",booking_id:booking.insertId})
+res.status(200).json({message:"your booking confirmed....",booking_id:booking.insertId})
     }catch(err){ 
-        res.send({message:'something went wrong....',booking:'failed',err:err});
+        res.status(400).json({message:"something went wrong.."}) 
     } 
 })
 
 app.post("/cancel_booking",async(req,res)=>{
     try{
 let [cancel] = await db.query("delete from bookings where user_id=? and event_id=?",[req.body.user_id,req.body.event_id]);
-res.send({message:'sucessfully deleted'})
+res.status(200).json({message:'sucessfully deleted'})
     }catch(err){
-        res.send({message:'something went wrong....'})
+        res.status(400).json({message:"something went wrong.."}) 
     }
 })
 
 app.post("/cancel_events",async(req,res)=>{
     try{
 let [delete_event] = await db.query("update events set status=false where id=? and user_id=?",[req.body.event_id,req.body.user_id]);
-res.send({message:"cancled sucessfully...."})
+res.status(200).json({message:"cancled sucessfully...."})
     }catch(err){
-        res.send({message:"something went wrong.."}) 
+        res.status(400).json({message:"something went wrong.."}) 
     }
 })
 
@@ -69,14 +64,13 @@ app.post("/events/:id/attendance",check_user,check_event,check_user_booking,asyn
          if (already.length > 0) {
          return res.send({ message: "Attendance already marked" });
     }
-         let data = await db.query("insert into event_attendance (user_id,event_id) values (?,?)",[req.body.user_id,event_id])
-         res.send({message:'sucessfully join'})
+        let data = await db.query("insert into event_attendance (user_id,event_id) values (?,?)",[req.body.user_id,event_id])
+         res.status(200).json({message:'sucessfully join'})
     }catch(err){
         console.log(err)
-               res.send({message:"something went wrong..",err:err})  
+        res.status(400).json({message:"something went wrong.."}) 
     }
 })
-
 let book = await db.query("select * from events")
 console.log(book)
 
